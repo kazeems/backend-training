8a6a0c1f5e3d00526c4fa9d9e23ead06eeec2ab8
kazeems <seidukazeemolawale@gmail.com>
2023-03-27 13:09:08 +0100

    api routes

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -13,6 +15,33 @@ class RegisterController extends Controller
         $request->validate([
             'first_name' => ['required', 'alpha', 'min:3', 'max:10'],
             'email' => ['required', 'string', 'min:3']
+        ]);
+    }
+
+    public function registerApi(Request $request) {
+        $params = $request->validate([
+            'name' => ['string', 'required', 'min:3'],
+            'email' => ['required', 'string', 'min:3'],
+            'password' => ['required', 'string', 'min:5'],
+        ]);
+
+        $name = $params['name'];
+        $email = $request->email;
+        $password = Hash::make($request->password);
+
+        User::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => $password
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => "Registration was successful and data stored!",
+            'data' => [
+                'submittedName' => $name,
+                'submittedEmail' => $email
+            ]
         ]);
     }
 }
--- database/migrations/2023_03_27_115221_create_user_api_migration.php
+++ database/migrations/2023_03_27_115221_create_user_api_migration.php
@@ -13,14 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('user_api_migration', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('email');
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps(); // created_at updated_at
+            $table->timestamps();
         });
     }
 
@@ -31,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('user_api_migration');
     }
 };
